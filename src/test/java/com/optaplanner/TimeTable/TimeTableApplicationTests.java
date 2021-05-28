b4c5d0ed9d82c7b113a0883bdf36c8d3c90cc98c
Ujjwaldhiman1 <Ujjwa.ldhiman@oodlestechnologies.com>
2021-05-28 17:01:15 +0530

    complete

---
+++ src/test/java/com/optaplanner/TimeTable/TimeTableApplicationTests.java
@@ -0,0 +1,13 @@
+package com.optaplanner.TimeTable;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimeTableApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
